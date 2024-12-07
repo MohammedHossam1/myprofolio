b38052a1ac69a97a3b77c83dadae4b85004100b6
mohammedhossam1 <mohammedhossam199998@gmail.com>
2024-12-07 13:59:50 +0200

    Removed the NavBar

--- src/App.js
+++ src/App.js
@@ -1,54 +1,45 @@
-import { useEffect, useState } from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import loading from '../src/assets/loadd.gif';
-import './App.css';
-import About from './Components/About/About';
-import Contact from './Components/Contact/Contact';
-import Home from './Components/Home';
-import Projects from './Components/Projects/Projects';
-import Skills from './Components/Skills/Skills';
+import { useEffect, useState } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import loading from "../src/assets/loadd.gif";
+import "./App.css";
+import About from "./Components/About/About";
+import Contact from "./Components/Contact/Contact";
+import Home from "./Components/Home";
+import Projects from "./Components/Projects/Projects";
+import Skills from "./Components/Skills/Skills";
 
 function App() {
   const [isLoading, setIsLoading] = useState(true);
 
-useEffect(() => {
-  const delay = 2400; 
-  const timer = setTimeout(() => {
+  useEffect(() => {
+    const delay = 2400;
+    const timer = setTimeout(() => {
       setIsLoading(false);
-  }, delay);
-  return () => clearTimeout(timer);
-}, []);
+    }, delay);
+    return () => clearTimeout(timer);
+  }, []);
 
   return (
     <div>
-    {isLoading ? (
-    <div className='d-flex w-100 vh-100 justify-content-center align-items-center'>
-    <img loading="lazy" className="inlineImage" src={loading} />
-    
+      {isLoading ? (
+        <div className="d-flex w-100 vh-100 justify-content-center align-items-center">
+          <img loading="lazy" className="inlineImage" src={loading} />
+        </div>
+      ) : (
+        <BrowserRouter>
+          <div className="container ">
+            <Routes>
+              <Route path="/" exact element={<Home />} />
+              <Route path="/about" exact element={<About />} />
+              <Route path="/projects" exact element={<Projects />} />
+              <Route path="/contact" exact element={<Contact />} />
+              <Route path="/skills" exact element={<Skills />} />
+            </Routes>
+          </div>
+          {/* <Footer/> */}
+        </BrowserRouter>
+      )}
     </div>
-      
-      
-    ) : (
-      
-    <BrowserRouter>
-    <div className='container ' >
-    <Routes>
-      <Route path='/' exact element={<Home />}/>
-      <Route path='/about' exact element={<About />}/>
-      <Route path='/projects' exact element={<Projects />}/>
-      <Route path='/contact' exact element={<Contact />}/>
-      <Route path='/skills' exact element={<Skills />}/>
-    </Routes>
-    </div>
-    {/* <Footer/> */}
-    </BrowserRouter>
-    )}
-</div>
-
-
-
-
-
   );
 }
 
--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -10,7 +10,6 @@ export default function Home() {
 
   return (
     <>
-      <NavBar />
       <section id="header">
         <Header />
       </section>
