e172ef8526b4a963a964b5d0bec055aac84700bb
mohammedhossam1 <mohammedhossam199998@gmail.com>
2025-09-03 03:33:04 +0200

    dws

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -69,7 +69,7 @@ export default function Contact() {
         viewport={{ once: true }}
       >
         GETIN<span className="about-span">TOUCH</span>
-        <div className="text-sm">يهمني اعرف نت مين الصراحه ونا مش هقول لحد ومبكرهش حد  
+        <div className="text-sm">مهو لو نت شخص مناسب ليا وزي م ف جمجمتي هخطبك يعني هعمل 
            </div>
       </motion.h1>
       {/* Title for large screens */}
@@ -81,7 +81,7 @@ export default function Contact() {
         transition={{ duration: 0.5 }}
       >
         GETIN<span>TOUCH</span>
-        <div className="text-sm">يهمني اعرف نت مين الصراحه ونا مش هقول لحد ومبكرهش حد  
+        <div className="text-sm">مهو لو نت شخص مناسب ليا وزي م ف جمجمتي هخطبك يعني هعمل 
            </div>
 
       </motion.h1>
