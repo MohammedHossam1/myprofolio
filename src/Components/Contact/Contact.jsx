5f36f99ddb2f6846542879f3e233f08242282fcd
Mohammed Hossam Mohammed <125377278+MohammedHossam1@users.noreply.github.com>
2025-09-06 02:01:21 +0300

    Update Contact.jsx

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -69,7 +69,7 @@ export default function Contact() {
         viewport={{ once: true }}
       >
         GETIN<span className="about-span">TOUCH</span>
-<div className="text-sm">هستفاد
+<div className="text-sm">😡
 </div>
       </motion.h1>
       {/* Title for large screens */}
@@ -81,7 +81,7 @@ export default function Contact() {
         transition={{ duration: 0.5 }}
       >
         GETIN<span>TOUCH</span>
-<div className="text-sm">هستفاد
+<div className="text-sm">😡
 </div>
 
       </motion.h1>
